a2b020430bda1c17101685f14658717ab42f8019
albeeralkhawri <king.alber93@gmail.com>
2023-12-02 00:33:48 +0000

    try the new script github link

--- js/maps.js
+++ js/maps.js
@@ -10,16 +10,16 @@ function initMap() {
 
   // HTML content for the info window
   const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice"></div>' +
-    '<h1 id="firstHeading" class="firstHeading">Software City</h1>' +
-    '<div id="bodyContent">' +
-    '<p><b>Software City</b> is a dedicated provider of digital solutions. We specialize in creating innovative websites, web applications, digital menus, and effective digital advertising campaigns.</p>' +
-    '<p>Our team is passionate about delivering high-quality products that meet the unique needs of our clients. Whether you need a dynamic website, a user-friendly web app, captivating digital menus, or impactful digital advertising, we have the expertise to bring your vision to life.</p>' +
-    '<p>Discover the power of digital with Software City. Contact us today to elevate your online presence and engage your audience effectively.</p>' +
-    '<p>Visit us at <a href="https://www.facebook.com/city.softeware/">Software City</a> to learn more.</p>' +
-    '</div>' +
-    '</div>';
+  '<div id="content">' +
+  '<div id="siteNotice"></div>' +
+  '<h1 id="firstHeading" class="firstHeading">Software City</h1>' +
+  '<div id="bodyContent">' +
+  '<p><b>Software City</b> is a dedicated provider of digital solutions. We specialize in creating innovative websites, web applications, digital menus, and effective digital advertising campaigns.</p>' +
+  '<p>Our team is passionate about delivering high-quality products that meet the unique needs of our clients. Whether you need a dynamic website, a user-friendly web app, captivating digital menus, or impactful digital advertising, we have the expertise to bring your vision to life.</p>' +
+  '<p>Discover the power of digital with Software City. Contact us today to elevate your online presence and engage your audience effectively.</p>' +
+  '<p>Visit us at <a href="https://www.facebook.com/city.softeware/" class="custom-link">Software City</a> to learn more.</p>' +
+  '</div>' +
+  '</div>';
 
   // Create an info window with the specified content
   const infowindow = new google.maps.InfoWindow({
